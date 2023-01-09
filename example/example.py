02f64706afaee9cea1a02e2e7d9c3d9e25bc5343
David Turland <david@turland.org>
2023-01-09 15:11:35 +0000

    completed tests, refactor, and linking to orig

---
+++ example/example.py
@@ -0,0 +1,14 @@
+from tolerant.toposort import toposort,CircularDependencyError
+from pprint import pprint
+
+def main():
+    expected = [{4}, {3}, {2}, {1}]
+    actual = list(toposort({1: {2},
+                            2: {3},
+                            3: {4},
+                       }))
+    pprint(expected)
+    pprint(actual)
+
+
+main()
